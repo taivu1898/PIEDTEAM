6ca2f28cc8fdbb83fe2ef85f1183d58fa689f7df
taivu1898 <taivu189824@gmail.com>
2024-09-14 12:00:33 +0700

    14/9/2024 12:00AM

---
+++ MERN/CH03-JS/part04-oop/02-Prototypal-inheritance.js
@@ -0,0 +1,7 @@
+// 02 Prototyppal - inheritance: kế thừa nguyên mẫu (kế thừa 2 object với nhau)
+// [[Prototype]] đọc đầy đủ: ngoặc vuông * 2 Prototype
+// trong bât cứ object nào thì cũng luôn có một thuộc tính ẩn tên là [[Prototype]]
+//[[Prototype]] là thuộc tính chứa thong tin tiền thuân của object đó
+//                 chứa hằng tạo ra nó | cha nó
+//  Ta không thể .[[Prototype]] được
+//  Muốn truy cập vào [[Prototype]] thì phải thông qua accessor property có tên là __proto__
