4737c93ba795e1136d481909cf7db4bea49dccc2
taivu1898 <taivu189824@gmail.com>
2024-09-22 00:57:03 +0700

    22/9/2024 12:56 AM️

--- MERN/CH03-JS/part04-oop/06-class-classInheritance.js
+++ MERN/CH03-JS/part04-oop/06-class-classInheritance.js
@@ -103,3 +103,234 @@ hue.showName();
 // ôn lại OOP
 // ôn lại method Arrau
 // ôn lại this
+
+// Cảnh giác this trong các method của class
+class Button {
+  constructor(value) {
+    this.value = value;
+  }
+  click() {
+    console.log("Giá trị là " + this.value);
+  }
+}
+
+let btn = new Button("Ahihi");
+
+//btn {
+//  value: "Ahhi"
+//  [[Prototype]]: Button.prototype => class Button
+//                                    constructor
+//                                    click()
+//}
+
+//btn.click();
+
+//anh muốn hàm click được chạy sau 3s thì sao ?
+//setTimeout(btn.click, 3000); // BUG: btn.click() là sai, sẽ chạy liền ngay lập tức nhưng kết quả đúng
+
+// ra undefined vì là window chạy vì trong click có this, mà khi công thức được gọi trễ thì không còn người gọi nữa
+// this => windown => window.value = undefined
+// Truyền vào btn.click là xài đúng
+// btn.click là công thức, và sau đó 3s thì công thức được lôi ra chạy
+
+//Cách 1:
+//setTimeout(() => {
+//  btn.click();
+//}, 3000);
+
+//Cách 2: bind
+
+//class Button1 {
+//  constructor(value) {
+//    this.value = value;
+//    this.click = this.click.bind(this);
+//  }
+//  click() {
+//    console.log("Giá trị là " + this.value);
+//  }
+//}
+//
+//let btn1 = new Button1("Ahihi");
+//setTimeout(btn1.click, 3000);
+
+//*btn1 {
+//  value: "Ahhi"
+//  click() được độ thêm có this
+//  [[Prototype]]: Button.prototype => class Button
+//                                    constructor
+//                                    click()
+//}
+
+//Cách 3: dùng arrow
+//class Button2 {
+//  constructor(value) {
+//    this.value = value;
+//  }
+//  click = () => {
+//    console.log("Giá trị là " + this.value);
+//  }; // NOTE: Đây là một câu lệnh `bind`
+//}
+//// BUG: bị sếp chửi
+//
+//let btn2 = new Button2("Ahihi");
+//setTimeout(btn2.click, 3000);
+
+//II. class inheritance: kế thừa bằng class
+//trước khi có class thì chúng ta chỉ có constructor function mà thôi
+//việc kế thừa chắc chắn không phải thông qua từ khóa 'extends'
+
+class Animal {
+  constructor(name) {
+    this.name = name;
+    this.speed = 0;
+  }
+  //method
+  run(speed) {
+    this.speed = speed;
+    console.log(`${this.name} runs with speed ${this.speed}`);
+  }
+  stop() {
+    this.speed = 0;
+    console.log(`${this.name} stands still`);
+  }
+}
+
+let ani = new Animal("Ahihi do cho");
+
+class Rabbit extends Animal {
+  constructor(name) {
+    super(name); // new Animal()
+  }
+  hide() {
+    console.log(`${this.name} hides !!!`);
+  }
+  stop() {
+    setTimeout(() => {
+      super.stop();
+    }, 1000);
+  }
+}
+
+let yellowRabbit = new Rabbit("yellowRabbit");
+//yellowRabbit.hide();
+//yellowRabbit.run(6);
+//yellowRabbit.stop();
+//ani.hide(); // BUG:
+
+//yellowRabbit: {
+//  name: "yellowRabbit",
+//  speed: 0,
+//  [[Prototype]]: Rabbit.prototype => class Animal
+//}
+
+console.log(yellowRabbit);
+
+//class field
+class Animal2 {
+  name = "isAnimal"; // class field
+  constructor() {
+    console.log(this.name);
+  }
+}
+
+//bên trong obj là property, còn bên trong class là class field
+
+class Rabbit2 extends Animal2 {
+  constructor() {
+    super();
+  }
+}
+
+let an2 = new Animal2(); // isAnimal
+let rb2 = new Rabbit2(); // isAnimal
+// class field k có kế thừa, k có override, chỉ có overWrite
+
+console.log(rb2);
+
+//8 - static: tĩnh, trong java static nghĩa là prop thuộc về class, instance được phép truy cập và sử dụng, dùng chung
+//Trong js, static nghĩa prop CHỈ thuộc về class, instance không được phép truy cập
+
+class User9 {
+  name = "Diep";
+  static name2 = "Lan";
+}
+
+let obj1 = new User9();
+console.log(obj1.name);
+console.log(obj1.name2); // BUG: undefined (java chạy được)
+console.log(User9.name2);
+
+// ----------------------------------------------
+
+class Article {
+  constructor(title, date) {
+    this.title = title;
+    this.date = date;
+  }
+
+  static compare(articleA, articleB) {
+    return articleA.date - articleB.date;
+  }
+}
+
+// Danh sách mấy bài báo lá cải
+let articleList = [
+  new Article("Hoài Linh để quên 14 tỷ trong ngân hàng", new Date(2023, 3, 6)),
+  new Article("Jack bán áo có chữ ký Messi để từ thiện", new Date(2023, 0, 6)),
+  new Article(
+    "Người mua áo Messi dùng tiền để từ thiện trẻ mồ côi",
+    new Date(2022, 8, 6),
+  ),
+];
+
+articleList.sort(Article.compare);
+
+console.log(articleList);
+//Access modifier : đây là đại diện của tính đóng gói trong OOP ở js
+
+//trong js chỉ chia ra 2 là Internal và External interface
+// Internal interface - phương thức và thuộc tính chỉ có thể được truy cập bên trong các phương thức trong class, không phải từ bên ngoài.
+// External interface - phương thức và thuộc tính có thể truy cập được từ ngoài và trong class.
+// Trong Javascript, có 2 loại thuộc tính và phương thức:
+
+// Public: có thể truy cập từ bất kỳ đâu. Nghĩa là external interface. Cho đến bây giờ thì chúng ta chỉ sử dụng thuộc tính public
+// Private: có thể truy cập bên trong class. Nghĩa là internal interface
+// Trong nhiều ngôn ngữ khác thì còn tồn tại trường "protected": chỉ có thể truy cập bên trong class và những class kế thừa.
+
+// Trường Protected không được quy định trong Javascript ở cấp độ ngôn ngữ, những trong thực tế để cho tiện lợi thì chúng ta có thể giả lập để quy ước với nhau.
+
+//ReadOnly
+//nếu khai báo get mà k có set, thì nó sẽ thành readOnly, không đổi giá trị đc
+//nếu không có set/get thì nó tự tạo , sẽ gán bt
+//các dev quy ước tên _ ở trước là private chỉ dùng trong class, nên truy cập bằng get/set
+//không nên . tới
+//việc quy ước này không đảm bảo được ngôn ngữ, chỉ là quy ước
+
+class CoffeeMachine {
+  constructor(power) {
+    this._power = power;
+  }
+  get power() {
+    return this._power;
+  }
+}
+
+let cfm = new CoffeeMachine(100);
+cfm.power = 1000;
+cfm._power = 1000; // thoải mái, người không thích là xếp
+console.log(cfm.power);
+
+class CoffeeMachine1 {
+  #power;
+  constructor(power) {
+    this.#power = power;
+  }
+  get power() {
+    return this.#power;
+  }
+}
+
+let cfm1 = new CoffeeMachine1(100);
+cfm1.power = 1000;
+//cfm1.#power = 1000; // NOTE: Mức độ ngôn ngữ
+console.log(cfm1.power);
